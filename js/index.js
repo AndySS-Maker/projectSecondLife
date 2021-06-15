3764eb43919ed25c9ea78ec36b749a1cd7805d0b
andyss <andy_ss_22@icloud.com>
2021-06-15 16:40:01 -0300

    projeto iniciado, fotos e textos inseridos

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const quadros = [
+    {img: 'interação 1', descrição: 'Imerso nesse universo caotico, todo dia temos que tomar decisoes, e estas nos levam para lugares completamente diferentes, ou não !' , escolha1: 'Tomar pirula azul', escolha2: 'Tomar pirula vermelha'}
+    {img: 'interação 2', descrição: ' Tarde de chuva para variar como sempre,  preciso ir trabalhar, pleno ano 7022 e ainda temos que fazer isso ! droga !!!', escolha1: 'Ir de posante', escolha2: 'Ir de busão'}
+    {img: 'interação 2.1', descrição: ' dia doido , chuvoso, mas não da para ficar nesse cubiculo que chamam de casa, bora dar um giro ...', escolha1: ' ir de busão espacial', escolha2: ' ir de hawaianas'}
+    {img: 'interação 3', descrição: 'Não adianta os carros voarem se ainda existe transito, merda !!! mas olha ,só que gata vou ver qual é a dela', escolha1: ' ir enquadrar '}
+    {img: 'interação 3.1', descrição: ' Busão cheio da porra, o mundo deveria ser uma maravilha no segundo milenio com so 7 bilhoes de habitantes, puta que pariu que gata vou chegar nela', escolha1: 'ir conversar com a  garota', escolha2: 'Sou feio deixa pra lá'}
+    {img: 'interação 3.2', descrição: 'Que chuva ducaraiii tenho que pegar o uber por conta do gesso no braço , me sinto tão sozinho no meio de 977 bilhões de pessoas,  mas pera ai que taxista robo gata', escolha1: 'chegar na taxista robo'}
+    {img: 'interação 4', descrição: ': Com a mina no metel , as coisas comecam a esquentar', escolha1: 'Não tenho camisinha, bora assim mesmo', escolha2: 'Ficar só nos amasso'}
+    {img: 'interação 4.1', descrição: 'Com a robo no carro, as coisas comecam a esquentar.', escolha1: 'Ficar só nos amasso', escolha2: ' trocar engrenagens e fluidos.'}
+    {img: 'interação 5', descrição: ' Esse Jacksosvaldo é minha cara , potsss estou 17 milliseconds atrasado para o trabalho', escolha1: 'Se apresar para o trabalhar'}
+    {img: 'interação 5.1', descrição: 'Nossa dollyneuza o 3XY22 é sua cara, logo vai completar 3 anos e vai poder trabalhar', escolha1: 'Se apresar para o trabalhar', escolha2: 'ir para o bar tomar comer cachaça'}
+    {img: 'interação 5.2', descrição: 'Não aguento mais essa vida vazia de bar , semana que vem vou comecar a ir para a igreja', escolha1: ' ir para o bar tomar comer cachaça'}
+    {img: 'interação 6', descrição: ' AAAAAAAAAAAAAAA não aguento mais chuva ! tenho que ir trabajar, vou de carro ou de onibus ?! hummm , como de costume de carro', escolha1: 'morreu em um car crash com um onibus'}
+    {img: 'interação 6.1', descrição: 'Hoje acordei bem Maria , não esta chovendo vou ir de onibus trabalhar  para mudar um pouco', escolha1: 'morreu tropecando na saida do busao'}
+];
+
