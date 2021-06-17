7f3c7f9a3e6c44f6db2272a51ab1b8fe724d2df2
andyss <andy_ss_22@icloud.com>
2021-06-17 01:49:17 -0300

    melhorias no projeto

--- js/index.js
+++ js/index.js
@@ -95,6 +95,12 @@ const scenes = [
     firstChoice: "Você Se Fud%$# !!!",
     secondChoice: "Recomeçar",
   },
+  {
+    img: "./img/paramental img.jfif",
+    description: "",
+    firstChoice: "Start",
+    secondChoice: "Game",
+  },
 ];
 
 let imgElement = document.getElementById("img_1");
@@ -133,118 +139,187 @@ class Step {
     });
   }
 }
-  
+
+let startStep = new Step(
+  scenes[14].img,
+  scenes[14].description,
+  scenes[14].firstChoice,
+  scenes[14].secondChoice,
+  () => {
+    InitialStep.populate();
+  },
+  () => {
+    InitialStep.populate();
+  }
+);
+
 let InitialStep = new Step(
   scenes[0].img,
   scenes[0].description,
   scenes[0].firstChoice,
   scenes[0].secondChoice,
-  () => {FirstStep.populate()},
-  () => {SecondStep.populate()}
-    );
+  () => {
+    FirstStep.populate();
+  },
+  () => {
+    SecondStep.populate();
+  }
+);
 let FirstStep = new Step(
   scenes[1].img,
   scenes[1].description,
   scenes[1].firstChoice,
   scenes[1].secondChoice,
-  () => {ThirdStep.populate()},
-  () => {FourthStep.populate()}
-  );
+  () => {
+    ThirdStep.populate();
+  },
+  () => {
+    FourthStep.populate();
+  }
+);
 let SecondStep = new Step(
   scenes[2].img,
   scenes[2].description,
   scenes[2].firstChoice,
   scenes[2].secondChoice,
-  () => {FourthStep.populate()},
-  () => {FifthStep.populate()}
-  );
+  () => {
+    FourthStep.populate();
+  },
+  () => {
+    FifthStep.populate();
+  }
+);
 let ThirdStep = new Step(
   scenes[3].img,
   scenes[3].description,
   scenes[3].firstChoice,
   scenes[3].secondChoice,
-  () => {SixthStep.populate()},
-  () => {SixthStep.populate()}
-  );
+  () => {
+    SixthStep.populate();
+  },
+  () => {
+    SixthStep.populate();
+  }
+);
 let FourthStep = new Step(
-    scenes[4].img,
-    scenes[4].description,
-    scenes[4].firstChoice,
-    scenes[4].secondChoice,
-    () => {SixthStep.populate()},
-    () => {SeventhStep.populate()}
-      );
+  scenes[4].img,
+  scenes[4].description,
+  scenes[4].firstChoice,
+  scenes[4].secondChoice,
+  () => {
+    SixthStep.populate();
+  },
+  () => {
+    SeventhStep.populate();
+  }
+);
 let FifthStep = new Step(
   scenes[5].img,
   scenes[5].description,
   scenes[5].firstChoice,
   scenes[5].secondChoice,
-  () => {SeventhStep.populate()},
-  () => {SeventhStep.populate()}
-  );
+  () => {
+    SeventhStep.populate();
+  },
+  () => {
+    SeventhStep.populate();
+  }
+);
 let SixthStep = new Step(
   scenes[6].img,
   scenes[6].description,
   scenes[6].firstChoice,
   scenes[6].secondChoice,
-  () => {EighthStep.populate()},
-  () => {NinthStep.populate()}
-  );
+  () => {
+    EighthStep.populate();
+  },
+  () => {
+    NinthStep.populate();
+  }
+);
 let SeventhStep = new Step(
   scenes[7].img,
   scenes[7].description,
   scenes[7].firstChoice,
   scenes[7].secondChoice,
-  () => {NinthStep.populate()},
-  () => {TenthStep.populate()}
-  );
+  () => {
+    NinthStep.populate();
+  },
+  () => {
+    TenthStep.populate();
+  }
+);
 let EighthStep = new Step(
   scenes[8].img,
   scenes[8].description,
   scenes[8].firstChoice,
   scenes[8].secondChoice,
-  () => {EleventhStep.populate()},
-  () => {EleventhStep.populate()}
+  () => {
+    EleventhStep.populate();
+  },
+  () => {
+    EleventhStep.populate();
+  }
 );
 let NinthStep = new Step(
   scenes[9].img,
   scenes[9].description,
   scenes[9].firstChoice,
   scenes[9].secondChoice,
-  () => {EleventhStep.populate()},
-  () => {TwelfthStep.populate()}
+  () => {
+    EleventhStep.populate();
+  },
+  () => {
+    TwelfthStep.populate();
+  }
 );
 let TenthStep = new Step(
   scenes[10].img,
   scenes[10].description,
   scenes[10].firstChoice,
   scenes[10].secondChoice,
-  () => {TwelfthStep.populate()},
-  () => {TwelfthStep.populate()}
+  () => {
+    TwelfthStep.populate();
+  },
+  () => {
+    TwelfthStep.populate();
+  }
 );
 let EleventhStep = new Step(
   scenes[11].img,
   scenes[11].description,
   scenes[11].firstChoice,
   scenes[11].secondChoice,
-  () => {ThirteenthStep.populate()},
-  () => {ThirteenthStep.populate()}
+  () => {
+    ThirteenthStep.populate();
+  },
+  () => {
+    ThirteenthStep.populate();
+  }
 );
 let TwelfthStep = new Step(
   scenes[12].img,
   scenes[12].description,
   scenes[12].firstChoice,
   scenes[12].secondChoice,
-  () => {ThirteenthStep.populate()},
-  () => {ThirteenthStep.populate()}  
+  () => {
+    ThirteenthStep.populate();
+  },
+  () => {
+    ThirteenthStep.populate();
+  }
 );
 let ThirteenthStep = new Step(
-    scenes[13].img,
-    scenes[13].description,
-    scenes[13].firstChoice,
-    scenes[13].secondChoice,
-  () => {InitialStep.populate()},
-  () => {InitialStep.populate()},
-  );
+  scenes[13].img,
+  scenes[13].description,
+  scenes[13].firstChoice,
+  scenes[13].secondChoice,
+  () => {
+    InitialStep.populate();
+  },
+  () => {
+    InitialStep.populate();
+  }
+);
 
-InitialStep.populate();
+startStep.populate();
