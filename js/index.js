4b7f9fc5892c2eea0c90e4889d444eeda57ee7f0
andyss <andy_ss_22@icloud.com>
2021-06-16 21:24:30 -0300

    melhorias no codigo

--- js/index.js
+++ js/index.js
@@ -79,12 +79,14 @@ const scenes = [
     description:
       " AAAAAAAAAAAAAAA não aguento mais chuva ! tenho que ir na locadora, vou de carro ou onibus ?! hummm , como de costume vou de carro",
     firstChoice: "morreu em um car crash com um onibus",
+    secondChoice: "To com fome vou beber alguma coisa",
   },
   {
     img: "./img/interação 6.1.gif",
     description:
       "Hoje acordei bem Maria , não esta chovendo vou ir de onibus trabalhar  para mudar um pouco",
     firstChoice: "morreu tropecando na saida do busao",
+    secondChoice: "To com fome vou beber alguma coisa",
   },
 ];
 
@@ -124,163 +126,110 @@ class Step {
     });
   }
 }
-
-function action() {
-  FirstStep.populate();
-}
-
-function action1() {
-    SecondStep.populate();
-  }
-
-function action2() {
-    ThirdStep.populate();
-}
-
-function action3() {
-    FourthStep.populate();
-  }
-
-  function action4() {
-    FifthStep.populate();
-  }
-  
-  function action5() {
-    SixthStep.populate();
-  }
-
-  function action6() {
-    SeventhStep.populate();
-  }
-
-  function action7() {
-    EighthStep.populate();
-  }
-
-  function action8() {
-    NinthStep.populate();
-  }
-
-  function action9() {
-    TenthStep.populate();
-  }
-
-  function action10() {
-    EleventhStep.populate();
-  }
-
-  function action11() {
-    TwelfthStep.populate();
-  }
-
-  function action12() {
-    ThirteenStep.populate();
-  }
-
   
 let InitialStep = new Step(
   scenes[0].img,
   scenes[0].description,
   scenes[0].firstChoice,
   scenes[0].secondChoice,
-  action,
-  action1
-);
+  () => {FirstStep.populate()},
+  () => {SecondStep.populate()}
+    );
 let FirstStep = new Step(
   scenes[1].img,
   scenes[1].description,
   scenes[1].firstChoice,
   scenes[1].secondChoice,
-  action2,
-  action3
-);
+  () => {ThirdStep.populate()},
+  () => {FourthStep.populate()}
+  );
 let SecondStep = new Step(
   scenes[2].img,
   scenes[2].description,
   scenes[2].firstChoice,
   scenes[2].secondChoice,
-  action3,
-  action4
-);
+  () => {FourthStep.populate()},
+  () => {FifthStep.populate()}
+  );
 let ThirdStep = new Step(
   scenes[3].img,
   scenes[3].description,
   scenes[3].firstChoice,
   scenes[3].secondChoice,
-  action5,
-  action5
-);
+  () => {SixthStep.populate()},
+  () => {SixthStep.populate()}
+  );
 let FourthStep = new Step(
     scenes[4].img,
     scenes[4].description,
     scenes[4].firstChoice,
     scenes[4].secondChoice,
-    action5,
-    action6
-  );
+    () => {SixthStep.populate()},
+    () => {SeventhStep.populate()}
+      );
 let FifthStep = new Step(
   scenes[5].img,
   scenes[5].description,
   scenes[5].firstChoice,
   scenes[5].secondChoice,
-  action6,
-  action6
-);
+  () => {SeventhStep.populate()},
+  () => {SeventhStep.populate()}
+  );
 let SixthStep = new Step(
   scenes[6].img,
   scenes[6].description,
   scenes[6].firstChoice,
   scenes[6].secondChoice,
-  action7,
-  action8
-);
+  () => {EighthStep.populate()},
+  () => {NinthStep.populate()}
+  );
 let SeventhStep = new Step(
   scenes[7].img,
   scenes[7].description,
   scenes[7].firstChoice,
   scenes[7].secondChoice,
-  action8,
-  action9
-);
+  () => {NinthStep.populate()},
+  () => {TenthStep.populate()}
+  );
 let EighthStep = new Step(
   scenes[8].img,
   scenes[8].description,
   scenes[8].firstChoice,
   scenes[8].secondChoice,
-  action10,
-  action10
+  () => {EleventhStep.populate()},
+  () => {EleventhStep.populate()}
 );
 let NinthStep = new Step(
   scenes[9].img,
   scenes[9].description,
   scenes[9].firstChoice,
   scenes[9].secondChoice,
-  action10,
-  action11
+  () => {EleventhStep.populate()},
+  () => {TwelfthStep.populate()}
 );
 let TenthStep = new Step(
   scenes[10].img,
   scenes[10].description,
   scenes[10].firstChoice,
   scenes[10].secondChoice,
-  action11,
-  action11
+  () => {TwelfthStep.populate()},
+  () => {TwelfthStep.populate()}
 );
 let EleventhStep = new Step(
   scenes[11].img,
   scenes[11].description,
   scenes[11].firstChoice,
   scenes[11].secondChoice,
-  action12,
-  action12
+  () => {FirstStep.populate()},
+  () => {FirstStep.populate()}
 );
 let TwelfthStep = new Step(
   scenes[12].img,
   scenes[12].description,
   scenes[12].firstChoice,
   scenes[12].secondChoice,
-  action12,
-  action12
+  () => {FirstStep.populate()},
+  () => {FirstStep.populate()}  
 );
 
 InitialStep.populate();
